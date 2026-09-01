72a577c2f1126af9d00dcce3ded32a3356c8f781
diego <diegofonterosa@gmail.com>
2026-09-01 15:45:58 +0200

    feat: añadir detección de secretos con Gitleaks

---
+++ src/config_example.php
@@ -0,0 +1,6 @@
+<?php
+// Fichero de ejemplo con secretos expuestos (INTENCIONADO para test)
+$db_password = "SuperSecretPass123!";
+$api_key = "AKIAIOSFODNN7EXAMPLE";
+$aws_secret = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
+?>
